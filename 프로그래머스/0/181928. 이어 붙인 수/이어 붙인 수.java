b3777bca1941e26d6c13d3c9ce052ca69bb660b8
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-04 11:36:32 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.04 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,20 +1,6 @@
 class Solution {
     public int solution(int[] num_list) {
-        String odd = "";
-        String even = "";
-        
-        for(int num : num_list){
-            // num이 홀수면 
-            if(num % 2 != 0){
-                odd += Integer.toString(num);
-            }
-            // num이 짝수면
-            else{
-                even += Integer.toString(num);
-            }
-        }
-        
-        return Integer.parseInt(odd) + Integer.parseInt(even);
-        
+        int answer = 0;
+        return answer;
     }
 }
