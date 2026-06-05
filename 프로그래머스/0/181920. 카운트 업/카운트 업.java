e9e13b874929d9f62bb6cc242492dee0446d89a6
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-05 12:12:20 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] arr = new int[end_num - start_num + 1];
+        
+        for(int i=0; i<arr.length; i++){
+            arr[i] = start_num + i;
+        }
+        
+        return arr;
+    }
+}
