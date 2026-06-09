1f8da4539c2583d283a8b24bae35c310a976ace0
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-09 16:17:51 +0900

    [level 0] Title: 배열 조각하기, Time: 0.06 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int start = 0;
+        int end = query.length - 1;
+                
+        for(int i=0; i<query.length; i++){        
+            if(i%2 == 0){
+                end = start + query[i];
+            }
+            else{
+                start = start + query[i];
+            }
+        }
+        
+        int[] result = new int[end - start + 1];
+        
+        for(int i=0; i<result.length; i++){
+            result[i] = arr[i+start];
+        }
+        
+        return result;
+    }
+}
