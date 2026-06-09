47e46dd9a529e31cd98d99f4bb22dd43585158e3
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-09 14:21:09 +0900

    [level 0] Title: 암호 해독, Time: 4.11 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String result = "";
+        
+        for(int i=0; i<cipher.length(); i++){
+            if((i+1)%code == 0){
+                result += cipher.charAt(i);
+            }
+        }
+        
+        return result;
+    }
+}
