1fdac6afd6d560ea27ac7c62770c0d96e99c0cb4
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-23 14:21:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 28.87 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String result = "";
+        
+        for(int i=0; i<n; i++){
+            if(i % 2 == 0){
+                result += '수';
+            }
+            else{
+                result += '박';
+            }
+        }
+        
+        return result;
+    }
+}
