8667cf076efda15ff6c71d348d8c552521fa9485
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-06-23 10:49:30 +0900

    [level 1] Title: 예산, Time: 5.09 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int cnt = 0;
+        
+        for(int i=0; i<d.length; i++){
+            if(budget < d[i]){
+                break;
+            }
+            
+            budget -= d[i];
+            cnt++;
+        }
+        
+        return cnt;
+    }
+}
