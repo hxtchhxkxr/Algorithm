419a5b24b7bb92979afcce8d04c4e9c946a97671
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-09-10 18:02:22 +0900

    [Passed] 2개씩 그룹짓기 (206ms, 13MB)

---
+++ Codetree/trail2/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[2 * n];
+
+        for (int i = 0; i < 2 * n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        int max = 0;
+
+        for(int i=0; i<n; i++){
+            int sum = nums[i] + nums[nums.length - i - 1];
+
+            max = Math.max(sum, max);
+        }
+
+        System.out.print(max);
+    }
+}
