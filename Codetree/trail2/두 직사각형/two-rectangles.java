f876c4e08cd573e7f42f7a874859d3313fdb619b
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2026-09-19 14:34:30 +0900

    [Passed] 두 직사각형 (131ms, 10MB)

---
+++ Codetree/trail2/두 직사각형/two-rectangles.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+        
+        boolean isOverlap = true;
+
+        if((x2 < a1) || (a2 < x1) || (b2 < y1) || (y2 < b1)){
+            isOverlap = false;
+        }
+
+        if(isOverlap){
+            System.out.print("overlapping");
+        } else {
+            System.out.print("nonoverlapping");
+        }
+    }
+}
